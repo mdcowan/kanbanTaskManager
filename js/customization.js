b45a2604ca6dada041655d6576983d1b28429a7d
mdcowan <mdcowan@student.fullsail.edu>
2019-07-30 19:55:44 -0400

    Customization code refactor

--- js/customization.js
+++ js/customization.js
@@ -2,16 +2,17 @@
 const page = document.querySelector('body');
 const themechange = page.querySelector("#themechange");
 const btn = page.querySelector('header > button');
-const theme1Btn = page.querySelector('#custom1');
-const theme2Btn = page.querySelector('#custom2');
+const theme1Btn = page.querySelector('#customtheme1');
+const theme2Btn = page.querySelector('#customtheme2');
 const theme3Btn = page.querySelector('#default');
+
+theme1Btn.addEventListener('click', themeChange);
+theme2Btn.addEventListener('click', themeChange);
+theme3Btn.addEventListener('click', themeChange);
 
 // function to open the modal window
 function openModal(){
     themechange.style.display = "block";
-    theme1Btn.addEventListener('click',changeToTheme1);
-    theme2Btn.addEventListener('click', changeToTheme2);
-    theme3Btn.addEventListener('click', changeDefault);
 }
 
 // function to close the modal window
@@ -21,48 +22,46 @@ function closeModal(event){
     }
 }
 
-function changeToTheme1(){
+function themeChange(e){
     if(localStorage.getItem('customColor')){
         localStorage.removeItem('customColor');
     }
-    let data = {
-        colorClass: "1"
-    };
+    let data;
+    let choice = e.target.id;
+
+    switch (choice){
+        case "customtheme1":
+            console.log(choice);
+            data = {
+                colorClass: "1"
+            };
+            page.classList.add('custom1');
+            if(page.classList.contains('custom2')){
+                page.classList.remove('custom2');
+            }
+            break;
+        case "customtheme2":
+            console.log(choice);
+            data = {
+                colorClass: "2"
+            };
+            page.classList.add('custom2');
+            if(page.classList.contains('custom1')){
+                page.classList.remove('custom1');
+            }
+            break;
+        case "default":
+            console.log(choice);
+            if(page.classList.contains('custom1')){
+                page.classList.remove('custom1');
+            }
+            if(page.classList.contains('custom2')){
+                page.classList.remove('custom2');
+            }
+            break;
+    }
+
     localStorage.setItem('customColor', JSON.stringify(data));
-
-    page.classList.add('custom1');
-    if(page.classList.contains('custom2')){
-        page.classList.remove('custom2');
-    }
-    themechange.style.display = "none";
-}
-
-function changeToTheme2(){
-    if(localStorage.getItem('customColor')){
-        localStorage.removeItem('customColor');
-    }
-    let data = {
-        colorClass: "2"
-    };
-    localStorage.setItem('customColor', JSON.stringify(data));
-
-    page.classList.add('custom2');
-    if(page.classList.contains('custom1')){
-        page.classList.remove('custom1');
-    }
-    themechange.style.display = "none";
-}
-
-function changeDefault(){
-    if(localStorage.getItem('customColor')){
-        localStorage.removeItem('customColor');
-    }
-    if(page.classList.contains('custom1')){
-        page.classList.remove('custom1');
-    }
-    if(page.classList.contains('custom2')){
-        page.classList.remove('custom2');
-    }
     themechange.style.display = "none";
 }
 
@@ -71,14 +70,15 @@ window.addEventListener('click', closeModal);
 
 function loadCustomColor(){
     let customColor = JSON.parse(localStorage.getItem('customColor'));
-    console.log(customColor);
+    let event = new MouseEvent('click');
+    console.log(event);
     if(customColor){
         switch(customColor.colorClass){
             case '1':
-                changeToTheme1();
+                theme1Btn.dispatchEvent(event);
                 break;
             case '2':
-                changeToTheme2();
+                theme2Btn.dispatchEvent(event);
                 break;
         }
     }
